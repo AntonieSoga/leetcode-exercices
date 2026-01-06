452d2b76a5c4eeb4640204059b6b2f363cfb6ca8
AntonieSoga <antoniesoga.work@gmail.com>
2026-01-06 18:01:28 +0200

    updated problem solution and description

--- 01_arrays_and_strings/01_merge_sorted_array/merge_sorted_array.py
+++ 01_arrays_and_strings/01_merge_sorted_array/merge_sorted_array.py
@@ -1,39 +1,25 @@
-def merge_1(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-    ans = []
-    i =j = 0
-    while i < m and j < n :
-        if nums1[i] < nums2 [j] :
-            ans.append(nums1[i])
-            i += 1
-        else:
-            ans.append(nums2[j])
-            j += 1
-        
-    ans.extend(nums1[i:m])
-    ans.extend(nums2[j:n])
+def merge (nums1: list[int], m, nums2: list[int], n) -> None :
     
-    return ans
-
-print(merge_1([1,2,3],3,[2,5,6],3))
-
-def merge_2(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-    end = m + n - 1
-
-    while m > 0 and n > 0:
-        if nums1[m-1] > nums2[n-1]:
+    end = n + m - 1
+    
+    while n and m :
+        if nums1[m-1] > nums2[n-1] :
             nums1[end] = nums1[m-1]
-            m -= 1
+            m-=1
         else:
             nums1[end] = nums2[n-1]
-            n -= 1
-        end -= 1
+            n-=1
+        end-=1
+        
+    while m :
+        nums1[end] = nums1[m-1]
+        m -= 1
+        end -=1
+        
+### testing
+nums1 = [1,3,7,0,0,0]
+nums2 = [2,4,6]
+n = m = 3
 
-    while n > 0:
-        nums1[end] = nums2[n-1]
-        n -= 1
-        end -= 1
-
-
-nums1 = [1,2,3,0,0,0]
-merge_2(nums1, 3, [2,5,7], 3)
+merge(nums1, m, nums2, n)
 print(nums1)
