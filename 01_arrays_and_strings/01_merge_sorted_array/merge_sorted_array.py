d9f1d7179b9b598c03963ca7a41e9a24e458ce01
AntonieSoga <antoniesoga.work@gmail.com>
2026-01-06 20:50:20 +0200

    refactored merge_sorted_array.py to match the other Solutions

--- 01_arrays_and_strings/01_merge_sorted_array/merge_sorted_array.py
+++ 01_arrays_and_strings/01_merge_sorted_array/merge_sorted_array.py
@@ -1,25 +1,18 @@
-def merge (nums1: list[int], m, nums2: list[int], n) -> None :
-    
-    end = n + m - 1
-    
-    while n and m :
-        if nums1[m-1] > nums2[n-1] :
-            nums1[end] = nums1[m-1]
-            m-=1
-        else:
-            nums1[end] = nums2[n-1]
-            n-=1
-        end-=1
-        
-    while m :
-        nums1[end] = nums1[m-1]
-        m -= 1
-        end -=1
-        
-### testing
-nums1 = [1,3,7,0,0,0]
-nums2 = [2,4,6]
-n = m = 3
+class Solution:
+    def merge (nums1: list[int], m, nums2: list[int], n) -> None :
 
-merge(nums1, m, nums2, n)
-print(nums1)
+        end = n + m - 1
+
+        while n and m :
+            if nums1[m-1] > nums2[n-1] :
+                nums1[end] = nums1[m-1]
+                m-=1
+            else:
+                nums1[end] = nums2[n-1]
+                n-=1
+            end-=1
+
+        while m :
+            nums1[end] = nums1[m-1]
+            m -= 1
+            end -=1
