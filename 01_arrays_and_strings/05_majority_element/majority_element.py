073ed96376b24e72afa4c81a729853405d9d15e6
AntonieSoga <antoniesoga.work@gmail.com>
2026-01-07 18:05:11 +0200

    updated solution for '169. Majority Element'

--- 01_arrays_and_strings/05_majority_element/majority_element.py
+++ 01_arrays_and_strings/05_majority_element/majority_element.py
@@ -1,15 +1,15 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
+        # Boyer-Moore Voting Algorithm
+        candidate = None
         count = 0
-        candidate = 0
 
         for num in nums:
+            # If count is 0, we pick a new candidate
             if count == 0:
                 candidate = num
             
-            if num == candidate:
-                count += 1
-            else:
-                count -= 1
-            
+            # Increment count if we see the candidate, otherwise decrement
+            count += (1 if num == candidate else -1)
+
         return candidate
